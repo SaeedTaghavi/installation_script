dbd671ea204247f0d4f3ed9649f831c44beb9560
saeed <saeed@saeed.saeed>
2020-03-09 13:11:43 +0330

    some other steps of installation added

--- install.sh
+++ install.sh
@@ -1,31 +1,48 @@
-# cd ./src
-# make clean
-# make compile
-# make run
-# cd ..
+#### Set the Server Timezone to Asia/Tehran
+sudo timedatectl set-timezone Asia/Tehran
+
+#### set hostname 
+sudo hostnamectl set-hostname YOURHOSTNAME
 
 
+#### change autologin config
+sudo sed 's/--autologin pi//' /etc/systemd/system/getty\@tty1.service.d/autologin.con|sudo tee ~/autologin.con
+sudo mv ~/autologin.con /etc/systemd/system/getty\@tty1.service.d/autologin.con
 
 
+#### change setting clock log
+sed 's/#LogLevel=info/LogLevel=notice/' /etc/systemd/system.conf | sudo tee /etc/systemd/system.conf 
 
 
+#### Download and Install the Latest Updates for the OS
+sudo apt update && sudo apt -y full-upgrade
 
+#### install mysql in noninteractive
+sudo ./install_mysql.sh
 
-# #### Set the Server Timezone to Asia/Tehran
-# sudo timedatectl set-timezone Asia/Tehran
+#### install phpMyAdmin in noninteractive
+sudo ./install_phpMyAdmin.sh
 
-# #### set hostname 
-# sudo hostnamectl set-hostname YOURHOSTNAME
+#### install mysql-client
+sudo apt install -y libmysqlclient-dev
 
+#### install ssl
+sudo apt-get install -y libssl-dev
 
-# #### change autologin config
-# sudo sed 's/--autologin pi//' /etc/systemd/system/getty\@tty1.service.d/autologin.con|sudo tee ~/autologin.con
-# sudo mv ~/autologin.con /etc/systemd/system/getty\@tty1.service.d/autologin.con
+#### install WiringNP:
+sudo git clone https://github.com/friendlyarm/WiringNP ~/WiringNP  
+sudo chmod 755 ~/WiringNP/build
+cd ~/WiringNP
+sudo ./build   
+gpio readall
+cd ~
 
+sudo rm -rf ~/WiringNP/
 
-# #### change setting clock log
-# sed 's/#LogLevel=info/LogLevel=notice/' /etc/systemd/system.conf | sudo tee /etc/systemd/system.conf 
+#### install g++ compiler:
+sudo apt -y install g++
 
+#### copy to crontab:
+(sudo crontab -l ; sudo  echo "@reboot /home/fa/start.sh >> /home/fa/cron_log_b.txt 2>&1")| sudo  crontab -
+(sudo crontab -l ; sudo  echo "*/5 * * * * /home/fa/start.sh >> /home/fa/cron_log_m.txt 2>&1")| sudo  crontab -
 
-# #### Download and Install the Latest Updates for the OS
-# sudo apt update && sudo apt -y full-upgrade
